72d05c8c17eb0018234aec85357311f895fa1acd
jonny <jonny.huang@chuxingpay.com>
2021-02-01 13:38:25 +0800

    1.0.4

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.nestTracerMiddleware = exports.NestTracer = void 0;
 const api_1 = require("@opentelemetry/api");
@@ -25,7 +6,7 @@ const node_1 = require("@opentelemetry/node");
 const tracing_1 = require("@opentelemetry/tracing");
 const exporter_jaeger_1 = require("@opentelemetry/exporter-jaeger");
 const core_1 = require("@opentelemetry/core");
-const get = __importStar(require("lodash.get"));
+const get = require('lodash.get');
 class NestTracer {
     constructor(serviceName, pkg, exporter) {
         const provider = new node_1.NodeTracerProvider();
